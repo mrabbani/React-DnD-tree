495f341ab399afde9d1c1ee5e1db47901056becb
mrabbani <mahbub.rucse@gmail.com>
2015-11-14 00:16:16 +0600

    Editing option partially added

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,13 +1,57 @@
 import React, {Component} from 'react';
 
 export default class Note extends Component {
-  constructor(props){
+
+  constructor(props) {
+
     super(props)
-    this.test = 'Lazy';
+    this.state = {
+      editing: false
+    };
+    this.finishEdit = this.finishEdit.bind(this);
+    this.checkEnter = this.checkEnter.bind(this);
+    this.edit = this.edit.bind(this);
   }
 
+  renderEdit(){
+
+    return (
+      <input  type="text"
+      autoFocus={true}
+      defaultValue={this.props.task}
+      onBlur={this.finishEdit}
+      onKeyPress={this.checkEnter}
+       />
+    );
+  }
+
+  checkEnter(e) {
+    if(e.key == "Enter") {
+      this.finishEdit(e);
+    }
+  }
+
+  finishEdit(e) {
+    this.setState({
+      editing: false
+    });
+  }
+
+  renderTask() {
+    return (
+      <div onClick={this.edit}>
+        {this.props.task}
+      </div>
+    );
+  }
+
+  edit() {
+     this.setState({
+       editing: true
+     });
+  }
   render() {
 
-    return <div> {this.props.task} </div>;
+    return <div> {this.state.editing ? this.renderEdit() : this.renderTask()} </div>;
   }
 }
